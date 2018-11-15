c352bee4d10d40bfacc3d86dbf53b7948df58c35
ziyou8 <7610649+ziyou8@users.noreply.github.com>
2018-11-15 17:44:19 +0800

    learn how to use pandas to save local file.

--- save_file.py
+++ save_file.py
@@ -7,8 +7,8 @@ r = requests.get(url).text
 s = etree.HTML(r)
 file = s.xpath('//div[@class="comment"]/p/span/text()')
 
-with open('pinglun.txt', 'w', encoding='utf-8') as f:
-    for i in file:
-        f.write(i)
-        f.write('/n')
+import pandas as pd
 
+df = pd.DataFrame(file)
+print(df.head())
+df.to_excel('pinglun.xlsx')
